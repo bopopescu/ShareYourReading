fe1cf9e395d48f54ff79e1eebedc28d300e16530
JeromeVal <62535300+jeromeVal@users.noreply.github.com>
2020-05-03 19:10:31 +0800

    comment 01

--- models.py
+++ models.py
@@ -3,7 +3,8 @@
 from exts import db
 from datetime import datetime
 
-
+# Four tables are required to store data in the database (MySQL)
+# The User table includes id, username, password and user_type
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -11,13 +12,13 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     user_type = db.Column(db.Integer, nullable=False)
 
-
+# The Book table includes book_id and book_name, each book contains many information which is showed in Post table
 class Book(db.Model):
     __tablename__ = 'book'
     book_id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(200), nullable=False)
 
-
+# The Post table includes post_id, book_id, author_id, post_title, post_content, post_time, comment_num, author and book
 class Post(db.Model):
     __tablename__ = 'post'
     post_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -33,7 +34,7 @@ class Post(db.Model):
     author = db.relationship('User', backref=db.backref('posts'))
     book = db.relationship('Book', backref=db.backref('posts'))
 
-
+# The Comment table includes comment_id, post_id, author_id, comment_content, post and author
 class Comment(db.Model):
     __tablename__ = 'comment'
     comment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
